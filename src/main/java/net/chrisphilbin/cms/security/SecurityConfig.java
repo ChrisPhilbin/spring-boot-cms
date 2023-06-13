6b1c269610be5ceaaaccfc6cec8b5d9674aafcbf
Chris Philbin <chrisp@runway.com.au>
2023-06-13 09:04:35 -0400

    Configuring cors

--- src/main/java/net/chrisphilbin/cms/security/SecurityConfig.java
+++ src/main/java/net/chrisphilbin/cms/security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package net.chrisphilbin.cms.security;
 
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import net.chrisphilbin.cms.security.filter.AuthenticationFilter;
 import net.chrisphilbin.cms.security.filter.ExceptionHandlerFilter;
@@ -19,6 +24,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @AllArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 
     CustomAuthenticationManager customAuthenticationManager;
@@ -30,11 +36,8 @@ public class SecurityConfig {
         authenticationFilter.setFilterProcessesUrl("/authenticate");
 
         http        
-            .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
-            .and()
-            .csrf().disable()
+            .cors().and()
             .authorizeRequests()
-            .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
             .antMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
             .antMatchers(HttpMethod.POST, SecurityConstants.RESET_PASSWORD_PATH).permitAll()
             .antMatchers(HttpMethod.GET, SecurityConstants.CHANGE_PASSWORD_PATH).permitAll()
@@ -47,7 +50,17 @@ public class SecurityConfig {
             .addFilter(authenticationFilter)
             .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        return http.build();
+        return http.csrf().disable().build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        source.registerCorsConfiguration("/**", config.applyPermitDefaultValues());
+        config.setExposedHeaders(Arrays.asList("Authorization"));
+        // source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
     
 }
